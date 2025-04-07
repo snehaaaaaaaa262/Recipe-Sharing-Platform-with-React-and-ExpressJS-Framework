4615a8ed50d6150a788221abc8b17581c0fe5b0a
Sneha <snehaganiga1527@gmail.com>
2025-04-07 21:02:12 +0530

    Backend

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import { userRouter } from "./routes/user.js";
+import { recipesRouter } from "./routes/recipes.js";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/auth", userRouter);
+app.use("/recipes", recipesRouter);
+
+mongoose.connect(
+  "mongodb+srv://sneha1527:sneha1527@clusterrec.4uqcjad.mongodb.net/rec-db?retryWrites=true&w=majority&appName=Clusterrec",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }).then(
+    console.log("DB connected...")
+  )
+
+app.listen(3001, () => console.log("Server started"));
